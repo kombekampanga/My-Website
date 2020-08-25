4adf2b72dd520d2fa4ee064954823336bef283fc
kombe <kombe.kampanga@hotmail.com>
2020-08-25 20:16:18 +1200

    Creating A diffrent website style
    I took out the home page and made a few changes (changed the font of the menu at the top and took out the emoji's)

--- mywebsite.js
+++ mywebsite.js
@@ -1,29 +1,29 @@
 
 
 // Functions to display the contents ribbon at the top of the page
-function showHome() {
-    document.getElementById("Home").style.display = "block"
-    document.getElementById("AboutMe").style.display = "none"
-    document.getElementById("Projects").style.display = "none"
-    document.getElementById("Contact").style.display = "none"
-    document.getElementById("GuestBook").style.display = "none"
+// function showHome() {
+//     document.getElementById("Home").style.display = "block"
+//     document.getElementById("AboutMe").style.display = "none"
+//     document.getElementById("Projects").style.display = "none"
+//     document.getElementById("Contact").style.display = "none"
+//     document.getElementById("GuestBook").style.display = "none"
 
-    document.getElementById("HomeTab").style.color = "#e4add6"
-    document.getElementById("AboutMeTab").style.color = "black"
-    document.getElementById("ProjectsTab").style.color = "black"
-    document.getElementById("ContactTab").style.color = "black"
-}
+//     document.getElementById("HomeTab").style.color = "#e4add6"
+//     document.getElementById("AboutMeTab").style.color = "black"
+//     document.getElementById("ProjectsTab").style.color = "black"
+//     document.getElementById("ContactTab").style.color = "black"
+// }
 
 function showAboutMe() {
 
     document.getElementById("AboutMe").style.display = "block"
-    document.getElementById("Home").style.display = "none"
+    // document.getElementById("Home").style.display = "none"
     document.getElementById("Projects").style.display = "none"
     document.getElementById("Contact").style.display = "none"
     document.getElementById("GuestBook").style.display = "none"
 
     document.getElementById("AboutMeTab").style.color = "#e4add6"
-    document.getElementById("HomeTab").style.color = "black"
+    // document.getElementById("HomeTab").style.color = "black"
     document.getElementById("ProjectsTab").style.color = "black"
     document.getElementById("ContactTab").style.color = "black"
 
@@ -33,13 +33,13 @@ function showAboutMe() {
 const showProjects = () => {
 
     document.getElementById("Projects").style.display = "block"
-    document.getElementById("Home").style.display = "none"
+    // document.getElementById("Home").style.display = "none"
     document.getElementById("AboutMe").style.display = "none"
     document.getElementById("Contact").style.display = "none"
     document.getElementById("GuestBook").style.display = "none"
 
     document.getElementById("ProjectsTab").style.color = "#e4add6"
-    document.getElementById("HomeTab").style.color = "black"
+    // document.getElementById("HomeTab").style.color = "black"
     document.getElementById("AboutMeTab").style.color = "black"
     document.getElementById("ContactTab").style.color = "black"
 }
@@ -47,7 +47,7 @@ const showProjects = () => {
 function showContact() {
 
     document.getElementById("Contact").style.display = "block"
-    document.getElementById("Home").style.display = "none"
+    // document.getElementById("Home").style.display = "none"
     document.getElementById("AboutMe").style.display = "none"
     document.getElementById("Projects").style.display = "none"
     document.getElementById("GuestBook").style.display = "none"
@@ -55,10 +55,10 @@ function showContact() {
     document.getElementById("ContactTab").style.color = "#e4add6"
     document.getElementById("AboutMeTab").style.color = "black"
     document.getElementById("ProjectsTab").style.color = "black"
-    document.getElementById("HomeTab").style.color = "black"
+    // document.getElementById("HomeTab").style.color = "black"
 
 
    
 }
 
-window.onload = showHome;
+window.onload = showAboutMe;
