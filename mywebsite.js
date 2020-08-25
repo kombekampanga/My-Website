ad6bf4396c5ae061b6ba04f9963c39f57a7b6275
kombekampanga <63346045+kombekampanga@users.noreply.github.com>
2020-08-25 17:53:44 +1200

    Adding initial Files for my Website

---
+++ mywebsite.js
@@ -0,0 +1,64 @@
+
+
+// Functions to display the contents ribbon at the top of the page
+function showHome() {
+    document.getElementById("Home").style.display = "block"
+    document.getElementById("AboutMe").style.display = "none"
+    document.getElementById("Projects").style.display = "none"
+    document.getElementById("Contact").style.display = "none"
+    document.getElementById("GuestBook").style.display = "none"
+
+    document.getElementById("HomeTab").style.color = "#e4add6"
+    document.getElementById("AboutMeTab").style.color = "black"
+    document.getElementById("ProjectsTab").style.color = "black"
+    document.getElementById("ContactTab").style.color = "black"
+}
+
+function showAboutMe() {
+
+    document.getElementById("AboutMe").style.display = "block"
+    document.getElementById("Home").style.display = "none"
+    document.getElementById("Projects").style.display = "none"
+    document.getElementById("Contact").style.display = "none"
+    document.getElementById("GuestBook").style.display = "none"
+
+    document.getElementById("AboutMeTab").style.color = "#e4add6"
+    document.getElementById("HomeTab").style.color = "black"
+    document.getElementById("ProjectsTab").style.color = "black"
+    document.getElementById("ContactTab").style.color = "black"
+
+   
+}
+
+const showProjects = () => {
+
+    document.getElementById("Projects").style.display = "block"
+    document.getElementById("Home").style.display = "none"
+    document.getElementById("AboutMe").style.display = "none"
+    document.getElementById("Contact").style.display = "none"
+    document.getElementById("GuestBook").style.display = "none"
+
+    document.getElementById("ProjectsTab").style.color = "#e4add6"
+    document.getElementById("HomeTab").style.color = "black"
+    document.getElementById("AboutMeTab").style.color = "black"
+    document.getElementById("ContactTab").style.color = "black"
+}
+
+function showContact() {
+
+    document.getElementById("Contact").style.display = "block"
+    document.getElementById("Home").style.display = "none"
+    document.getElementById("AboutMe").style.display = "none"
+    document.getElementById("Projects").style.display = "none"
+    document.getElementById("GuestBook").style.display = "none"
+
+    document.getElementById("ContactTab").style.color = "#e4add6"
+    document.getElementById("AboutMeTab").style.color = "black"
+    document.getElementById("ProjectsTab").style.color = "black"
+    document.getElementById("HomeTab").style.color = "black"
+
+
+   
+}
+
+window.onload = showHome;
